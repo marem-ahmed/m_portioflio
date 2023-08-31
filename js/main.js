3d98c3ff4ea40bc7b8095a4b9e603b13d358449f
marem-ahmed <136582570+marem-ahmed@users.noreply.github.com>
2023-08-31 23:39:32 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var Typed = new Typed(".input",{
+    strings:["Front End Developer"],
+    typedSpeed:70,
+    backSpeed:90,
+    loop:true
+})
+const main=document.getElementsByClassName(".slections").addEventListener("click",function(){
+    document.body.scrollTop+=100;
+})
